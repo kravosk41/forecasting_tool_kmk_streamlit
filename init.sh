86159181564036ef273820a2b8a8622751dd96d0
kravosk41 <83767470+kravosk41@users.noreply.github.com>
2024-11-15 15:52:32 +0530

    Update init.sh
    scitkit-learn- init file update

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 pip install pandas==2.1.4
 pip install streamlit==1.30.0
 pip install numpy==1.26.3
-pip install scikit-learn==1.4.1
+pip install scikit-learn==1.4.1.post1
 pip install statsmodels==0.14.1
 pip install scipy==1.12.0
 pip install prophet==1.1.5
