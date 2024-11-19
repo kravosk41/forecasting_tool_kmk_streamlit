df84c5b1607afe75d14cceca3408f65e387247a1
kravosk41 <83767470+kravosk41@users.noreply.github.com>
2024-11-19 12:40:27 +0530

    optimized init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-pip install pandas==2.1.4
-pip install streamlit==1.30.0
-pip install numpy==1.26.3
-pip install scikit-learn==1.4.1.post1
-pip install statsmodels==0.14.1
-pip install scipy==1.12.0
-pip install prophet==1.1.5
-pip install plotly==5.24.1
-pip install openpyxl==3.1.2
+pip install pandas==2.1.4 streamlit==1.30.0 numpy==1.26.3 scikit-learn==1.4.1.post1 \
+          statsmodels==0.14.1 scipy==1.12.0 prophet==1.1.5 plotly==5.24.1 openpyxl==3.1.2
