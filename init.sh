baffdbebb2a3a125a3912ed45639f454ad7c9dd6
kravosk41 <venombyte4414@gmail.com>
2024-11-22 12:35:11 +0530

    added xlsxwriter to init

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 pip install pandas==2.1.4 streamlit==1.30.0 numpy==1.26.3 scikit-learn==1.4.1.post1 \
-          statsmodels==0.14.1 scipy==1.12.0 prophet==1.1.5 plotly==5.24.1 openpyxl==3.1.2
+          statsmodels==0.14.1 scipy==1.12.0 prophet==1.1.5 plotly==5.24.1 openpyxl==3.1.2 \
+          xlsxwriter==3.2.0
